e2ac9057db3a35113d1ebcc1f75e676a52d7bf34
unknown <mintychocolate101@gmail.com>
2020-07-20 11:29:31 -0700

    Updates

--- COSMIC/src/Function/Constants.java
+++ COSMIC/src/Function/Constants.java
@@ -10,6 +10,7 @@ public class Constants {
 	 * Change this to match your file output directory
 	 */
 	public static final String out_path = "D:\\Mitali\\ML\\NPS20\\test-out";
+	
 	/**
 	 * Change this to match your file input directory
 	 */
